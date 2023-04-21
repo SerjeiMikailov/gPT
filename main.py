b16d1dfe04839452b5bb1dcd3a535a6bc20f300e
SerjeiMikailov <felipemaders8@gmail.com>
2023-04-20 23:39:34 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,60 @@
+import requests
+import speech_recognition as sr
+from key import api_key
+
+def generate_text(prompt):
+    
+    url = "https://api.openai.com/v1/engines/davinci/completions"
+    api_key
+    headers = {
+        "Authorization": f"Bearer {api_key}",
+        "Content-Type": "application/json"
+    }
+    data = {
+        "prompt": prompt,
+        "max_tokens": 500,
+        "n": 1,
+        "stop": None,
+        "temperature": 1.0,
+    }
+
+    response = requests.post(url, headers=headers, json=data)
+
+    if response.status_code != 200:
+        print(f"Erro na requisição: {response.status_code}")
+        print(response.text)
+        return None
+
+    response_json = response.json()
+
+    if "choices" not in response_json:
+        print("A resposta não contém a chave 'choices'.")
+        return None
+
+    generated_text = response_json["choices"][0]["text"]
+    return generated_text
+
+if __name__ == "__main__":
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Say something!")
+        audio = r.listen(source)
+
+    try:
+        text_prompt = r.recognize_google(audio)
+        print("Google Speech Recognition thinks you said " + text_prompt)
+    except sr.UnknownValueError:
+        print("Google Speech Recognition could not understand audio")
+        text_prompt = None
+    except sr.RequestError as e:
+        print("Could not request results from Google Speech Recognition service; {0}".format(e))
+        text_prompt = None
+
+    if text_prompt:
+        result = generate_text(text_prompt)
+        if result:
+            print(result)
+        else:
+            print("Não foi possível gerar o texto.")
+    else:
+        print("Não foi possível converter o áudio em texto.")
